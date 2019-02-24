78aef17e8bd0e6741019d2fe1e6cfc7f07c85e69
puc_dong <1417766861@qq.com>
2019-02-24 21:36:25 +0800

    project init

---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
--- Travel/src/router/index.js
+++ Travel/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
+// @src目录
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },
   ]
 })
