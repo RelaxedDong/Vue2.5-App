799fd4934aecda0aa73668b0168da8b9eee297a9
puc_dong <1417766861@qq.com>
2019-02-25 14:45:03 +0800

    add icons

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -2,15 +2,18 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+
 export default {
   name: 'Home',
-  components: {HomeHeader, HomeSwiper}
+  components: {HomeHeader, HomeSwiper, HomeIcons}
 }
 </script>
 
---
+++ Travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,118 @@
+<template>
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="( page,index) of Mypages" :key="index">
+      <div class="icons">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </div>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+      },
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        }, {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        }, {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        }, {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '溜娃儿'
+        }, {
+          id: '0005',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/0334cf5430b9b5505fd79e2b8d7e8670.png',
+          desc: '爬长城'
+        }, {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+          desc: '故宫'
+        }, {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+          desc: '茶馆相声'
+        }, {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+          desc: '北京滑雪'
+        }, {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+          desc: '动植物园'
+        }
+      ]
+    }
+  },
+  computed: {
+    Mypages: function () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        console.log(pages)
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons
+    height:0
+    width 100%
+    padding-bottom 50%
+    overflow hidden
+    .icon
+      position relative
+      overflow hidden
+      float left
+      height 0
+      width 25%
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        box-sizing border-box
+        padding .1rem
+        left 0
+        right 0
+        bottom .44rem
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+    .icon-desc
+      position absolute
+      bottom 0
+      line-height .44rem
+      height .44rem
+      left 0
+      right 0
+      color $darkTextColor
+      text-align center
+      ellipsis()
+</style>
