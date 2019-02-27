75016d3ed5832a5947718975a1eeb3319db531d7
puc_dong <1417766861@qq.com>
2019-02-27 13:37:12 +0800

    search finish

--- Travel/src/pages/city/City.vue
+++ Travel/src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :cities="cities" :hotcities="hotCities" :letter="letter"></city-list>
     <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
--- Travel/src/pages/city/components/Search.vue
+++ Travel/src/pages/city/components/Search.vue
@@ -1,14 +1,64 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input type="text" v-model="keyword" class="search-input" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="(city,index) in cityList" :key="index">{{city}}</li>
+        <li class="search-item border-bottom" v-show="hasCity">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: ['cities'],
+  data: function () {
+    return {
+      keyword: '',
+      cityList: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasCity () {
+      return !this.cityList.length
+    }
+  },
+  watch: {
+    keyword: function () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        if (!this.keyword) {
+          this.cityList = []
+          return
+        }
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value.name)
+            }
+          })
+          this.cityList = result
+        }
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
+
 <!--组件样式，不影响其他组件-->
 <!--1rem = html front-size = 50px-->
 <style lang="stylus" scoped>
@@ -25,4 +75,18 @@ export default {
       width 100%
       text-align center
       border-radius .06rem
+  .search-content
+    overflow hidden
+    background #eee
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    z-index: 1
+    bottom 0
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      color #666
+      background #fff
 </style>
